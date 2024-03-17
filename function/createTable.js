534b5c7eb090ed764bb7d46f5c40a5f52031c11f
MaxBased <hoseintahan84@gmail.com>
2024-03-17 08:43:12 +0330

    changed  query params

--- function/createTable.js
+++ function/createTable.js
@@ -2,5 +2,5 @@ const connection=require('../utils/connection');
 
 
 exports.CreateTable=async ()=>{
-    await connection.query('CREATE TABLE IF NOT EXISTS advertising (ad_Id INT PRIMARY KEY AUTO_INCREMENT, ad_Title VARCHAR(35),ad_Content TEXT(10000),isSubmit VARCHAR(1),date VARCHAR(10),user_Id INT, FOREIGN KEY (user_Id) REFERENCES `user`(`user_Id`))');
+    await connection.query('CREATE TABLE IF NOT EXISTS advertising (ad_Id INT PRIMARY KEY AUTO_INCREMENT, ad_Title VARCHAR(35),ad_Content TEXT(10000),isSubmit VARCHAR(1),date VARCHAR(10),price FLOAT,user_Id INT, FOREIGN KEY (user_Id) REFERENCES `user`(`user_Id`))');
 }
