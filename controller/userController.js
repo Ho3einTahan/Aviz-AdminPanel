e265c5351925d6f073f1799faf6ac8d75bdd804c
MaxBased <hoseintahan84@gmail.com>
2024-03-17 08:45:29 +0330

    changed  [users/login-admin]  userController.js

--- controller/userController.js
+++ controller/userController.js
@@ -1,6 +1,6 @@
 const connection = require('../utils/connection');
 const validation = require('../utils/validator');
-const jwt=require('jsonwebtoken');
+const jwt = require('jsonwebtoken');
 
 
 exports.RegisterUser = (req, res) => {
@@ -33,9 +33,9 @@ exports.LoginUser = async (req, res) => {
   const { phoneNumber, melliCode } = req.body;
 
   const [isExist, field] = await connection.query('SELECT phoneNumber,melliCode FROM user WHERE phoneNumber=? AND melliCode=?', [phoneNumber, melliCode]);
-  
-  const token = jwt.sign({ phoneNumber: phoneNumber, melliCode: melliCode },process.env.SECRETKEY, { expiresIn: '1h' });
-  if (isExist.length != 0) return res.status(200).send({ 'message': "ورود موفقیت آمیز بود",'token':token});
+
+  const token = jwt.sign({ phoneNumber: phoneNumber, melliCode: melliCode }, process.env.SECRETKEY, { expiresIn: '1h' });
+  if (isExist.length != 0) return res.status(200).send({ 'message': "ورود موفقیت آمیز بود", 'token': token });
 
   return res.status(400).send({ 'message': "کاربری با این اطلاعات وجود ندارد" });
 }
@@ -44,7 +44,22 @@ exports.LoginUser = async (req, res) => {
 
 
 exports.LoginAdmin = async (req, res) => {
+  const isSubmit = req.query.is_submit; // دریافت مقدار پارامتر is_submit
 
- return res.render('index3');
+  let advertising;
+
+  if (isSubmit == '1' || isSubmit==undefined) {
+    const [ad, field] = await connection.query(`SELECT * FROM user JOIN advertising a ON user.user_Id = a.user_Id AND a.isSubmit=1;`);
+    advertising = ad;
+  }
+  else {
+    const [ad, field] = await connection.query(`SELECT * FROM user JOIN advertising a ON user.user_Id = a.user_Id AND a.isSubmit=0;`);
+    advertising = ad;
+  }
+
+    return res.render('index3', {
+      'advertising': advertising,
+      params:isSubmit==0?0:1,
+    });
 
 }
